c096e5f06de387d874ea3d4ddbdb2beb30daacef
dmitriydligach <ddligach@luc.edu>
2024-11-08 09:15:40 -0600

    put text generation in a loop

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import transformers, torch, os
+from sympy.physics.units import temperature
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # model
@@ -9,13 +10,13 @@ device_map={ "": "cuda:7" } # i.e. put entire model on GPU 7
 model_path = f'/home1/shared/Models/Llama3/Llama-3.1-{lama_size}-Instruct'
 
 # configuration
-
+do_sample = False
+t = 1.0
+top_p = 1
+max_new_tokens = 100
 
 def main():
   """Ask for input and feed into llama2"""
-
-  prompt = [{"role": "system", "content": "You are a helpful assistant, that responds as a pirate."},
-            {"role": "user", "content": "What's Deep Learning?"}]
 
   tokenizer = AutoTokenizer.from_pretrained(model_path)
 
@@ -24,20 +25,26 @@ def main():
     device_map=device_map)
 
   generator = transformers.pipeline(
-    'text-generation',
+    task='text-generation',
     model=model,
     tokenizer=tokenizer,
     torch_dtype=torch.bfloat16,
     device_map='auto')
 
-  generated_outputs = generator(
-    prompt,
-    do_sample=False,
-    temperature=1.0,
-    top_p=1,
-    max_new_tokens=50)
+  while True:
 
-  print(generated_outputs[0]['generated_text'])
+    user_input = input("Please enter some text: ")
+    prompt = [{"role": "system", "content": "You are a helpful assistant."},
+              {"role": "user", "content": user_input}]
+
+    generated_outputs = generator(
+      prompt,
+      do_sample=do_sample,
+      temperature=t,
+      top_p=top_p,
+      max_new_tokens=max_new_tokens)
+
+    print(generated_outputs[0]['generated_text'][2]['content'])
 
 if __name__ == "__main__":
 
