2cf1512335788c63581e863b553d512b829d3068
Krishna <114757733+i-krishna@users.noreply.github.com>
2025-04-12 18:52:28 -0400

    Update fine-tune-llm.py

--- fine-tune-llm.py
+++ fine-tune-llm.py
@@ -51,3 +51,26 @@ dataset = DatasetDict({
     'validation': validation_dataset
 })
 
+# load dataset
+dataset = load_dataset('shawhin/imdb-truncated')
+dataset
+
+# display % of training data with label=1
+np.array(dataset['train']['label']).sum()/len(dataset['train']['label'])
+
+## Model
+
+from transformers import AutoModelForSequenceClassification
+model_checkpoint = 'distilbert-base-uncased'
+# model_checkpoint = 'roberta-base' # you can alternatively use roberta-base but this model is bigger thus training will take longer
+
+# define label maps
+id2label = {0: "Negative", 1: "Positive"}
+label2id = {"Negative":0, "Positive":1}
+
+# generate classification model from model_checkpoint
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id)
+
+# display architecture
+model
