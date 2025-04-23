df98a81bf8742c167e23d2b3dd8d86afbcc1505c
Krishna <114757733+i-krishna@users.noreply.github.com>
2025-04-23 17:26:08 -0400

    Update fine-tune-deepseek-medical-data.py

--- fine-tune-deepseek-medical-data.py
+++ fine-tune-deepseek-medical-data.py
@@ -1,7 +1,9 @@
 # Fine tune DeepSeek-R1 on Medical COT Dataset. 
 # Medical reasoning and clinical case analysis using LoRA (Low-Rank Adaptation) with Unsloth.
 
-# Ref: https://huggingface.co/develops20/DeepSeek-R1-Distill-Llama-8B-Medical-COT 
+# Dataset: https://huggingface.co/datasets/FreedomIntelligence/medical-o1-reasoning-SFT
+# Base Model: https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Llama-8B 
+# FIne-tuned model example: https://huggingface.co/develops20/DeepSeek-R1-Distill-Llama-8B-Medical-COT 
 
 # Create API in Hugging_Face & weights_and_biases. Link them to Kaggle Notebook. 
 from kaggle_secrets import UserSecretsClient
@@ -60,3 +62,72 @@ model, tokenizer = FastLanguageModel.from_pretrained(
     load_in_4bit=load_in_4bit, # Load the model in 4-bit quantization to save memory
     token=hugging_face_token, # Use hugging face token
 )
+
+# Testing DeepSeek R1 on a medical use-case before fine-tuning
+# Define a system prompt 
+
+prompt_style = """Below is an instruction that describes a task, paired with an input that provides further context. 
+Write a response that appropriately completes the request. 
+Before answering, think carefully about the question and create a step-by-step chain of thoughts to ensure a logical and accurate response.
+
+### Instruction:
+You are a medical expert with advanced knowledge in clinical reasoning, diagnostics, and treatment planning. 
+Please answer the following medical question. 
+
+### Question:
+{}
+
+### Response:
+<think>{}"""
+
+# Creating a test medical question for inference
+question = """A 61-year-old woman with a long history of involuntary urine loss during activities like coughing or 
+              sneezing but no leakage at night undergoes a gynecological exam and Q-tip test. Based on these findings, 
+              what would cystometry most likely reveal about her residual volume and detrusor contractions?"""
+
+# Enable optimized inference mode for Unsloth models (improves speed and efficiency)
+FastLanguageModel.for_inference(model)  # Unsloth has 2x faster inference!
+
+# Format the question using the structured prompt (`prompt_style`) and tokenize it
+inputs = tokenizer([prompt_style.format(question, "")], return_tensors="pt").to("cuda")  # Convert input to PyTorch tensor & move to GPU
+
+# Generate a response using the model
+outputs = model.generate(
+    input_ids=inputs.input_ids, # Tokenized input question
+    attention_mask=inputs.attention_mask, # Attention mask to handle padding
+    max_new_tokens=1200, # Limit response length to 1200 tokens (to prevent excessive output)
+    use_cache=True, # Enable caching for faster inference
+)
+
+# Decode the generated output tokens into human-readable text
+response = tokenizer.batch_decode(outputs)
+
+# Extract and print only the relevant response part (after "### Response:")
+print(response[0].split("### Response:")[1])  
+
+## NOTE: Before starting fine-tuning — why are we fine-tuning in the first place?
+## Even without fine-tuning, above model successfully generated a chain of thought and provided reasoning before delivering the final answer. 
+## The reasoning process is encapsulated within the <think> </think> tags.  So, why do we still need fine-tuning? 
+## The reasoning process, while detailed, was long-winded and not concise. Additionally, we want the final answer to be consistent in a certain style.
+
+# Updated training prompt style to add </think> tag 
+train_prompt_style = """Below is an instruction that describes a task, paired with an input that provides further context. 
+Write a response that appropriately completes the request. 
+Before answering, think carefully about the question and create a step-by-step chain of thoughts to ensure a logical and accurate response.
+
+### Instruction:
+You are a medical expert with advanced knowledge in clinical reasoning, diagnostics, and treatment planning. 
+Please answer the following medical question. 
+
+### Question:
+{}
+
+### Response:
+<think>
+{}
+</think>
+{}"""
+
+# Download the dataset using Hugging Face — function imported using from datasets import load_dataset
+dataset = load_dataset("FreedomIntelligence/medical-o1-reasoning-SFT","en", split = "train[0:500]",trust_remote_code=True) # Keep only first 500 rows
+dataset
