1ac2623aa8d8d75c37402c4bea6da227f5b2b6ab
Krishna <114757733+i-krishna@users.noreply.github.com>
2025-07-29 03:56:25 -0400

    Update test_model.py

--- Image Classifier_End_to_End ML Architecture/test_model.py
+++ Image Classifier_End_to_End ML Architecture/test_model.py
@@ -2,8 +2,8 @@ import requests
 
 url = 'http://localhost:5000/predict' 
 
-# or Deplot to AWS Sagemaker & infer that end point 
-# Ex: https://github.com/i-krishna/Business-Analytics/blob/main/Data-Science/Python/ai_model_integration.py
+# or Deploy to AWS Sagemaker & infer that end point 
+# Ref: https://github.com/i-krishna/Business-Analytics/blob/main/Data-Science/Python/ai_model_integration.py
 # https://github.com/i-krishna/Business-Analytics/blob/main/Data-Science/Python/aws_model_training.py 
 
 files = {'file': open('test_image.jpg', 'rb')}
